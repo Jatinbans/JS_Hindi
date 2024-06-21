0b5017e41695f91573ad1ec947aa8915ed00fe73
Jatin Bansal <96127218+Jatinbans@users.noreply.github.com>
2024-06-21 06:56:21 +0000

    varibales and datatypes done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountNumber = 78555
+let accountName = "Ram"
+var accountEmail = "j@gmail.com"
+accountPass = "ffgf"
+let accountType;
+
+/* prefer not to use var in coding */
+accountName = "mohan"
+accountEmail="m@gmail.com"
+accountPass = "efd"
+
+console.table([accountNumber,accountName,accountEmail,accountPass,accountType])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // this means treat all js code as a new version 
+
+//alert() it throws error coz we are using node js not browser 
+//code readiablity is priority
+
+
+let name = "mohan"
+let age = 45
+let isLoggedIn = false
+let state;
+
+// null is standalone value datatype also (empty value) null is also null 
+//undefined => value is not defined 
+// number is ranged from 2 to power 53
+//string ""
+//boolean true/false
+//symbol => uniques 
+
+
+//object 
+ console.log(typeof null); // object
+ console.log(typeof undefined); // undefined
