1bfe665a803ce2ace036ce1ab98e155c5e2463fa
Jatin Bansal <96127218+Jatinbans@users.noreply.github.com>
2024-06-21 08:16:38 +0000

    conversion done

---
+++ 01_basics/03_conversionsOperations.js
@@ -0,0 +1,29 @@
+// let score = "353er" // NaN
+// let score = null // 0
+// let score = undefined // NaN
+// let score = true // 1
+// let score = false // 0 
+// let score = "jatin" // NaN
+
+
+// console.log(typeof score); 
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score) // class based hai eh  // conversion aa eh 
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // ik special format aa check krn lyi 
+
+// let isloggedIn = 1
+// let booleanIsLoggedIn = Boolean(isloggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 - true
+//0- false
+//"" - false
+//"jatin" - true
+
+
+// let someNumber = 33 
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
