8b41128e45709ffe74e0f00ce1f369087b924303
Jatin Bansal <96127218+Jatinbans@users.noreply.github.com>
2024-06-21 18:26:10 +0000

    minor chnages

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,32 +1,51 @@
-//singleton constructor di help nal
-const tinderUser = new Object()
-tinderUser.id = "123asd"
-tinderUser.name = "sammy"
-tinderUser.islogin = false
+// //singleton constructor di help nal
+// const tinderUser = new Object()
+// tinderUser.id = "123asd"
+// tinderUser.name = "sammy"
+// tinderUser.islogin = false
 
-// console.log(tinderUser);
+// // console.log(tinderUser);
 
-const regularUser = {
-    email: "some@gmail.com",
-    fullname:{
-        userfullname: {
-            firstname : "jatin",
-            lastname:"bansal"
-        }
-    }
+// const regularUser = {
+//     email: "some@gmail.com",
+//     fullname:{
+//         userfullname: {
+//             firstname : "jatin",
+//             lastname:"bansal"
+//         }
+//     }
+// }
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+// const obj1 = {1:"a",2:"b"}
+// const obj2 = {3:"c",4:"d"}
+
+// // const obj3 = {...obj1,...obj2}
+
+// const obj3 =Object.assign({},obj1,obj2)
+// console.log(obj3);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+// console.log(tinderUser.hasOwnProperty('islogin'));
+
+
+
+// const course = {
+//     coursename:"js in hindi",
+//     price: "999",
+//     courseIns: "hitesh"
+
+// }
+
+// const {courseIns : ins} = course    //destructuring
+// console.log(ins);
+
+//----------API-------------------
+
+{
+    // json key - value - strings javascript object notation
+    
 }
-
-console.log(regularUser.fullname.userfullname.firstname);
-
-const obj1 = {1:"a",2:"b"}
-const obj2 = {3:"c",4:"d"}
-
-// const obj3 = {...obj1,...obj2}
-
-const obj3 =Object.assign({},obj1,obj2)
-console.log(obj3);
-
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
-console.log(tinderUser.hasOwnProperty('islogin'));
