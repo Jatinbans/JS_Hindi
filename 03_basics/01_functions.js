cb1135b577520468034dd27dabde48cc62574c7f
Jatin Bansal <96127218+Jatinbans@users.noreply.github.com>
2024-06-22 08:14:49 +0000

    advanced functions done

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,40 +1,72 @@
 
-function sayMyName(){
-    console.log("j");   // this is function defination
-
-}
-
-
-// sayMyName // function reference
-//() execution 
-
-// sayMyName()
-
-
-//function di defination ch ande aa perimeters(input)
-// function addtwoNum(num1,num2){
-//     console.log(num1 + num2);
+// function sayMyName(){
+//     console.log("j");   // this is function defination
 
 // }
 
-function addtwoNum(num1,num2){
-   let result = num1+num2
-   return result
 
+// // sayMyName // function reference
+// //() execution 
+
+// // sayMyName()
+
+
+// //function di defination ch ande aa perimeters(input)
+// // function addtwoNum(num1,num2){
+// //     console.log(num1 + num2);
+
+// // }
+
+// function addtwoNum(num1,num2){
+//    let result = num1+num2
+//    return result
+
+// }
+// const result = addtwoNum(4,5)
+// console.log("result: ", result);
+// //calling time te arguments
+// addtwoNum(4,5) // argument 
+
+// function loginUserMessage(username){
+//     if(username === undefined){
+//         console.log("please enter a username");
+//         return
+//     }
+//     return `${username} just logged in `
+// }
+
+// // console.log(loginUserMessage("jatin"))
+
+// console.log(loginUserMessage()) // undefined
+
+
+function calculatecartPrice(...num1){  // rest operator array vich patia sarria values
+    return num1
 }
-const result = addtwoNum(4,5)
-console.log("result: ", result);
-//calling time te arguments
-addtwoNum(4,5) // argument 
 
-function loginUserMessage(username){
-    if(username === undefined){
-        console.log("please enter a username");
-        return
-    }
-    return `${username} just logged in `
+console.log(calculatecartPrice(2,400,600));
+
+const user = {
+    username: "jatin",
+    price: 299
 }
 
-// console.log(loginUserMessage("jatin"))
+function handleObject(anyObject){
+    console.log(`username is ${anyObject.username} and price is ${anyObject.price}`);
+}
 
-console.log(loginUserMessage()) // undefined
+// handleObject(user)
+
+// handleObject({
+//     username: "sam",
+//     price:455
+// })
+
+
+const array = [200,400,500]
+function secondValue(getArray){
+    return getArray[2]
+}
+// console.log(secondValue(array));
+
+console.log(secondValue([200,400,500]));
