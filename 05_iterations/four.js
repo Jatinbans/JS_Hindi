56afc94cbdefa989274a0861ab1cf146500d1330
Jatin Bansal <96127218+Jatinbans@users.noreply.github.com>
2024-06-24 10:10:54 +0000

    loops

---
+++ 05_iterations/four.js
@@ -0,0 +1,23 @@
+// const myobject = {
+//     js:'javascript',
+//     cpp: 'c++',
+//     rb: 'ruby',
+//     swift: 'swift by Apple'
+// }
+
+// // for (const key in myobject) {
+// //     console.log(key);
+// // }
+
+// // for (const key in myobject) {
+// //     console.log(myobject[key]);
+// // }
+
+// for (const key in myobject) {
+//     console.log(`${key} shortcut is for ${myobject[key]}`);
+// }
+
+const programming = ["js","py","java","cpp"]
+for (const key in programming) {
+   console.log(key);
+}
---
+++ 05_iterations/three.js
@@ -0,0 +1,38 @@
+//array specific loops
+// for of loop 
+
+
+// const arr = [1,2,3,4,5]
+// for (const i of arr) {
+//     console.log(`value of ${i}`);
+// }
+
+// const greetings = "Hello world!"
+// for(const greet of greetings){
+//     if(greet == " ") continue
+//     console.log(greet);
+// }
+
+
+// Maps ik object hai
+// const map = new Map()
+// map.set('IN',"India")
+// map.set('US',"United State of America")
+// map.set('CN',"Canada")
+// map.set('CN',"Canada")
+// // console.log(map);
+
+
+// for (const [key, value] of map) {
+//     console.log(key, ':-', value);
+// }
+
+
+// const myObj = {          not iteratable
+//     'game1' : 'NFS',
+//     'game2' : 'Spiderman'
+// }
+
+// for (const [key, value] of myObj) {
+//     console.log(key, ':-', value);
+// }
