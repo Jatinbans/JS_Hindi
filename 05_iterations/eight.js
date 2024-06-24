170bf91e4d74ea9f05c424eda9ed3f514f997f4a
Jatin Bansal <96127218+Jatinbans@users.noreply.github.com>
2024-06-24 17:34:52 +0000

    filter and reduce done

---
+++ 05_iterations/eight.js
@@ -0,0 +1,41 @@
+// reduce
+// const myNums = [1,2,3]
+// // const myTotal = myNums.reduce(function (acc, currVal) {
+// //     return acc + currVal
+// // },0)
+
+// const myTotal = myNums.reduce((acc,curval) => acc+curval ,0)
+// console.log(myTotal);
+
+
+// const shoppingKart = [
+//     {
+//         itemName: "js course",
+//         price: 2333
+
+//     },
+//     {
+//         itemName: "js course",
+//         price: 2333
+
+//     },
+//     {
+//         itemName: "py course",
+//         price: 1599
+
+//     },
+//     {
+//         itemName: "mobile dev course",
+//         price: 5999
+
+//     },
+//     {
+//         itemName: "data science course",
+//         price: 12999
+
+//     }
+// ]
+
+// const priceToPay = shoppingKart.reduce ((acc, item) => acc+item.price,0)
+// console.log(priceToPay);
+
