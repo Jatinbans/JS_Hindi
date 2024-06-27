8ef28d85c31a1078a05c8be1e189ebd15421eb1f
Jatin Bansal <96127218+Jatinbans@users.noreply.github.com>
2024-06-27 13:21:10 +0000

    project 1 done and 2 is going

---
+++ 07_projects/2-BMICalculator/index.js
@@ -0,0 +1,29 @@
+const form = document.querySelector('form')
+//this usecase will gave empty value
+//const height=parseInt(document.querySelector('#height').value)
+
+form.addEventListener('submit',function(e){
+    e.preventDefault()
+    const height=parseInt(document.querySelector('#height').value)
+    const weight=parseInt(document.querySelector('#weight').value)
+    const results= document.querySelector('#results')
+
+    if(height === '' || height <0 || isNaN(height)){
+        results.innerHTML = `Please give a valid height ${height}`;
+    } else if(weight === '' || weight <0 || isNaN(weight)){
+        results.innerHTML = `Please give a valid height ${weight}`;
+    }else {
+       const bmi =  (weight/((height * height)/10000)).toFixed(2)
+        results.innerHTML = `<span> ${bmi}</span>`;
+
+        if(bmi <18.6){
+            results.innerHTML = "You are under Weight"
+        } else if(bmi > 24.9){
+            results.innerHTML = "You are over Weight"
+        }else{
+            results.innerHTML = "You are in normal range"
+        }
+    }
+
+
+})
---
+++ 07_projects/proectspart1.md
@@ -0,0 +1,12 @@
+//mark up file
+# Projects related to DOM
+## project link 
+[click here] (https://stackbliz.com/edit/dom-project-chaiaurcode?file-index.html)
+
+# solution code
+
+## project1
+
+```javascript
+console.log("jatin")
+```
